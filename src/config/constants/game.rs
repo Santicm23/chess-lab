75c18495fef1405499c66a1b905da67f7fda37a4
Santicm23 <santicastrom@outlook.com>
2024-05-29 09:56:44 -0500

    adding test suites for piece movements

--- src/config/constants/game.rs
+++ src/config/constants/game.rs
@@ -4,6 +4,27 @@ pub enum Color {
     BLACK,
 }
 
+impl Color {
+    pub fn opposite(&self) -> Color {
+        match self {
+            Color::WHITE => Color::BLACK,
+            Color::BLACK => Color::WHITE,
+        }
+    }
+}
+
+/// implement clone
+impl Clone for Color {
+    fn clone(&self) -> Color {
+        match self {
+            Color::WHITE => Color::WHITE,
+            Color::BLACK => Color::BLACK,
+        }
+    }
+}
+
+impl Copy for Color {}
+
 /// Represents the type of a chess piece.
 pub enum PieceType {
     PAWN,
